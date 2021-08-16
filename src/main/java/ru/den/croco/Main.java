ddb4b7a6353217418c7fb3f4fd80f81a241e8fce
dkochanovskiy <dkochanovskiy@gmail.com>
2021-08-16 15:27:08 +0300

    Parser for xml(in work)

--- src/main/java/ru/den/croco/Main.java
+++ src/main/java/ru/den/croco/Main.java
@@ -12,45 +12,65 @@ public class Main {
 
         HashSet<String> setRow = new HashSet<>();
         HashSet<String> setDuplicate = new HashSet<>();
-        HashSet<String> setCity = new HashSet<>();
+        List<HashMap<String, String>> cityFloor = new ArrayList<>();
 
-        System.out.println("Enter the path to the file");
-        filePath = reader.readLine();
+        while (true) {
+            System.out.println("\nВведите путь до файла");
+            filePath = reader.readLine();
 
-        try {
-            File file = new File(filePath);
-            Scanner fileReader = new Scanner(file);
+            try {
+                File file = new File(filePath);
+                Scanner fileReader = new Scanner(file);
 
-            while (fileReader.hasNextLine()) {
-                row = fileReader.nextLine();
-                listRow.add(row);
-                
-                String[] cities = row.split(";");
+                while (fileReader.hasNextLine()) {
+                    row = fileReader.nextLine();
 
-                setCity.add(cities[0].substring(1, cities[0].length() - 1));
+                    if (filePath.startsWith("csv", filePath.length() - 3)) {
+                        if (!("\"city\";\"street\";\"house\";\"floor\"".equals(row))) {
+//                            String[] cities = row.split(";");
 
-                if (!setRow.add(row)) {
-                    setDuplicate.add(row);
+//                            setCity.add(cities[0].substring(1, cities[0].length() - 1));
+                        }
+                    }
+
+                    if (filePath.startsWith("xml", filePath.length() - 3)) {
+                        if (!("<root>".equals(row)) && !("</root>".equals(row)) &&
+                                !("<?xml version=\"1.0\" encoding=\"utf-8\"?>".equals(row))) {
+                            listRow.add(row);
+                        }
+                    }
+
+                    if (!setRow.add(row)) {
+                        setDuplicate.add(row);
+                    }
                 }
+
+                // вывод
+                if (setDuplicate.size() != 0) {
+                    for (String duplicate : setDuplicate) {
+                        System.out.println("Дубликат: " + duplicate + ", количество повторений: " +
+                                Collections.frequency(listRow, duplicate));
+                    }
+                } else {
+                    System.out.println("Дубликатов не обнаружено");
+                }
+
+                for (String element : listRow) {
+                    String city = element.substring(12);
+                    city = city.substring(0, city.indexOf("\""));
+
+                    String floor = element.substring(element.indexOf("floor") + 7, element.length() - 4);
+                    HashMap<String, String> one_store = new HashMap<>();
+                    one_store.put(city, floor);
+                    cityFloor.add(one_store);
+                    System.out.println(cityFloor);
+                }
+
+                //конец вывода
+                fileReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Файл по указанному пути не найден");
             }
-
-            fileReader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
-
-        if (setDuplicate.size() == 0) {
-            System.out.println("No duplicate lines found");
-        } else {
-            for (String duplicate : setDuplicate) {
-                System.out.println("Duplicate: " + duplicate + ", number of occurrences: " +
-                        Collections.frequency(listRow, duplicate));
-            }
-        }
-
-        for (String city : setCity) {
-            System.out.println(city);
-        }
-
     }
 }
